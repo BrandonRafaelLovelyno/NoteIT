51a4c9e5ece77ab89f924e2365bf0c60a7ce49a1
Venamora <stevennamorarohasaraginapitu@mail.ugm.ac.id>
2024-11-12 22:25:08 +0700

    menambahkan home, navbar, dan footer, serta beberapa logic

---
+++ app/login/page.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const loginUser = () => {
+  return (
+    <div>
+        <h1>Login</h1>
+        <div>
+            Enter your Email :
+        </div>
+        <div>
+            <button>
+                <a href="/">Back</a>
+            </button>
+        </div>
+    </div>
+  )
+
+}
+
+export default loginUser
